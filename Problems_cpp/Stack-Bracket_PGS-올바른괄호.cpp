ed37da855c1eb175052347032c39b91919fdc446
sahyunjin <tkguswls1106@naver.com>
2023-11-26 23:47:49 +0900

    디렉토리 정리중. 네이밍 변경.

---
+++ Problems_cpp/Stack-Bracket_PGS-올바른괄호.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s)
+{    
+    stack<char> st;
+    for(char ch : s) {
+        if(ch == '(') st.push(ch);
+        else {  // ch == ')'
+            if(st.empty()) return false;
+            else st.pop();
+        }
+    }
+    if(st.size() != 0) return false;
+
+    return true;
+}
+
+int main()
+{
+    string str1 = solution("(())()") ? "true" : "false";
+    string str2 = solution(")()(") ? "true" : "false";
+    string str3 = solution("())((()))(()") ? "true" : "false";
+
+    cout << str1 << "\n";  // => true 출력
+    cout << str2 << "\n";  // => false 출력
+    cout << str3 << "\n";  // => false 출력
+
+    return 0;
+}
