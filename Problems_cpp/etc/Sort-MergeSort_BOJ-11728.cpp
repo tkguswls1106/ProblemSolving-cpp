95635cb272d756055ae5290b485b76b6d9b706bc
sahyunjin <tkguswls1106@naver.com>
2024-01-15 22:48:00 +0900

    Sort-MergeSort_BOJ-11728 풀기 완료.

---
+++ Problems_cpp/etc/Sort-MergeSort_BOJ-11728.cpp
@@ -0,0 +1,64 @@
+#include <vector>
+#include <deque>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n,m;
+    cin >> n >> m;
+
+    // 합병정렬MergeSort는 각 구역이 정렬된 상태로 시작해야하지만,
+    // 어차피 이 문제에서는 정렬된 상태로 각각의 배열을 입력받으므로, 따로 이부분은 코드작성 해줄필요 없다.
+    deque<int> a;
+    deque<int> b;
+    vector<int> resultV;
+
+    for(int i=0; i<n; i++) {
+        int input;
+        cin >> input;
+        a.push_back(input);
+    }
+    for(int i=0; i<m; i++) {
+        int input;
+        cin >> input;
+        b.push_back(input);
+    }
+
+    while(true) {
+        if(!a.empty() && !b.empty()) {
+            if(a.front() < b.front()) {
+                resultV.push_back(a.front());
+                a.pop_front();
+            }
+            else {
+                resultV.push_back(b.front());
+                b.pop_front();
+            }
+        }
+        else if(a.empty()) {
+            if(!b.empty()) {
+                for(int num : b) resultV.push_back(num);
+                break;
+            }
+            else break;
+        }
+        else if(b.empty()) {
+            if(!a.empty()) {
+                for(int num : a) resultV.push_back(num);
+                break;
+            }
+            else break;
+        }
+    }
+
+    for(int num : resultV) cout << num << " ";
+
+
+    return 0;
+}
