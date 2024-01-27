8019774cbb792de50b5ef7c01156ebfbc4ea6327
sahyunjin <tkguswls1106@naver.com>
2024-01-27 18:34:10 +0900

    BOJ-1874 풀기 완료.

---
+++ Problems_cpp/ps/BOJ-1874.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+// 문제 지문이 별로다. 만약 지문이 이해가 안된다면, 'www.youtube.com/watch?v=byCxMbgzEVM&t=19s' 를 참고하자.
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, input;
+    cin >> n;
+    stack<int> st;
+    vector<char> v;
+
+    int maxPush = 0;
+    while(n--) {
+        cin >> input;
+
+        if(!st.empty() && st.top() == input) {
+            st.pop();
+            v.push_back('-');
+            continue;
+        }
+
+        if(input > maxPush) {
+            for(int i=maxPush+1; i<=input; i++) {
+                st.push(i);
+                v.push_back('+');
+            }
+            st.pop();
+            v.push_back('-');
+            maxPush = input;
+        }
+        else {  // input < maxPush 일때
+            cout << "NO";
+            return 0;
+        }
+    }
+
+    for(char ch : v) {
+        cout << ch << "\n";
+    }
+    
+
+    return 0;
+}
