a87410ab347af2558e8a37ad9953bbd31d0a9639
sahyunjin <tkguswls1106@naver.com>
2024-02-11 22:42:26 +0900

    DP 관련 두 문제 풀이 및 정리 완료.

---
+++ Problems_cpp/DP-Fibonacci_BOJ-2748.cpp
@@ -0,0 +1,36 @@
+// - DP '피보나치 수열' 문제 -
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+// 참고로 이 문제에서는
+// '0번째 피보나치 수는 0이고, 1번째 피보나치 수는 1이다. n이 주어졌을 때, n번째 피보나치 수를 구하라.'
+// 라고 명시되어있으므로, dp[0]=0 으로 작성해도 좋다.
+
+long long dp[92] = { 0, };  // 피보나치 수열은 N이 커질수록 값이 급속도로 증가하므로 자료형을 long long으로 사용해야한다.
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    // DP 점화식 초기값 할당.
+    // 피보나치의 경우에는, 점화식이 'DP[i] = DP[i-1] + DP[i-2]'로써 두가지의 초기값 할당이 필요하다.
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for(int i=2; i<=n; i++) {  // DP 테이블에 미리 값을 채워줌.
+        dp[i] = dp[i-1] + dp[i-2];  // 'DP[i] = DP[i-2] + DP[i-1]'로 생각하면 더 편할지도.
+    }
+
+    cout << dp[n];
+
+
+    return 0;
+}
