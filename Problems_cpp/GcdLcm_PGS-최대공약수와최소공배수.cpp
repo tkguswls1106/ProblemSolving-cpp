0d1e50c9747b2c1b23c550851a8aed4eeeca3ca0
sahyunjin <tkguswls1106@naver.com>
2024-03-05 15:30:07 +0900

    GcdLcm_PGS-최대공약수와최소공배수 풀기 완료

---
+++ Problems_cpp/GcdLcm_PGS-최대공약수와최소공배수.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) {  // 최대공약수
+    if(b == 0) return a;
+    return gcd(b, a%b);
+}
+
+int lcm(int a, int b) {  // 최소공배수
+    return a*b / gcd(a,b);
+}
+
+vector<int> solution(int n, int m) {
+    vector<int> answer;
+    answer.push_back(gcd(n,m));
+    answer.push_back(lcm(n,m));
+    
+    return answer;
+}
+
+int main()
+{
+    vector<int> answer1 = solution(3, 12);
+    vector<int> answer2 = solution(2, 5);
+
+    for(int num : answer1) cout << num << " ";  // => 3 12 출력
+    cout << "\n";
+    for(int num : answer2) cout << num << " ";  // => 1 10 출력
+    cout << "\n";
+
+
+    return 0;
+}
