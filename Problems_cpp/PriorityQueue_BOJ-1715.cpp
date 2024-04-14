28d2b73d6190af5144ad5837fa94a31796ce933b
sahyunjin <tkguswls1106@naver.com>
2024-04-15 02:35:00 +0900

    PriorityQueue_BOJ-1715 풀기 완료

---
+++ Problems_cpp/PriorityQueue_BOJ-1715.cpp
@@ -0,0 +1,41 @@
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+// - 사용할 알고리즘 찾기:
+// 가장 작은 묶음 2개를 하나로 뭉쳐야하는것을 계속 반복해야한다.
+// => '오름차순으로 정렬해서 앞의 두가지를 합치고, 이를 push하고, 다시 정렬하는것'을 반복해야한다.
+// => multiset보단 우선순위큐가 더 편리할것으로 예상됨.
+// ==> 우선순위큐 사용.
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    priority_queue<int, vector<int>, greater<int>> pq;  // 최소힙 우선순위큐 (오름차순)
+
+    int n, input;
+    cin >> n;
+    int answer = 0;
+    while(n--) {
+        cin >> input;
+        pq.push(input);
+    }
+
+    while(pq.size() > 1) {
+        int n1 = pq.top();
+        pq.pop();
+        int n2 = pq.top();
+        pq.pop();
+        pq.push(n1 + n2);
+        answer += (n1 + n2);
+    }
+    
+    cout << answer;
+
+
+    return 0;
+}
