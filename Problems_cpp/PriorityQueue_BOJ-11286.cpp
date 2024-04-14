eb2bf9951d7c90125b520a34275ee8f8f3df3c2b
sahyunjin <tkguswls1106@naver.com>
2024-04-15 01:24:12 +0900

    PriorityQueue_BOJ-11286 풀기 완료

---
+++ Problems_cpp/PriorityQueue_BOJ-11286.cpp
@@ -0,0 +1,47 @@
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+// - 사용할 알고리즘 찾기:
+// "'가장' 작은 숫자를 출력하고, 이를 배열에서 제외한다." 라고 문제에서 명시했으므로,
+// 이는 우선순위큐가 적합함을 알 수 있다.
+
+struct cmp {
+    bool operator() (int a, int b) {
+        if(abs(a) != abs(b)) {
+            return abs(a) > abs(b);  // 일반적인 커스텀 정렬 cmp때와는 반대로 작성해야한다. 주의하기!!
+        }
+        else {
+            return a > b;  // 일반적인 커스텀 정렬 cmp때와는 반대로 작성해야한다. 주의하기!!
+        }
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    priority_queue<int, vector<int>, cmp> pq;
+    
+    int cnt, x;
+    cin >> cnt;
+    while(cnt--) {
+        cin >> x;
+        if(x != 0) pq.push(x);
+        else {
+            if(pq.empty()) {
+                cout << 0 << "\n";
+            }
+            else {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        }
+    }
+
+
+    return 0;
+}
